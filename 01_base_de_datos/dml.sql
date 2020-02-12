eae59bbd65a2b994b1f6ee39f4e1a09660a84c83
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2020-02-12 13:24:28 -0700

    Guardar nuevo usuario
    Se guardo un nuevo usuario en la tabla

---
+++ 01_base_de_datos/dml.sql
@@ -0,0 +1,3 @@
+-- DML (Data Manipulation Language)
+INSERT INTO users (name, passwd, email, curp, firstname, lastname)
+VALUES ('bidkar','123','bidkar@cetis108.edu.mx','lksdjflkdsjlk','Bidkar','Aragon');
